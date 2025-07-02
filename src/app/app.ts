0a43eadc5495c75f5341410dafb57a15f4aeeaff
L3me05 <lemueldimarcantonio.elite@gmail.com>
2025-07-02 16:31:26 +0200

    feat: primi componenti

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Navbar} from './core/components/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
+    <app-navbar />
+    <div class="max-w-screen-lg mx-3 lg:mx-auto">
+      <router-outlet />
+    </div>
   `,
   styles: [],
+  styleUrls: ['./app.css']
 })
 export class App {
   protected title = 'corso-front-end';
